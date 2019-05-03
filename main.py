781281a0dec0c32b866271ec059ce9638bf9aec3
Alex Still <stillalex55@gmail.com>
2019-05-02 22:21:53 -0500

    Added error messages and fixed title error

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:Aa02850621!@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://buildablog:Aa02850621!@localhost:8889/buildablog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'jfhjerj3h234'
@@ -28,13 +28,22 @@ def index():
 def new_post():
     if request.method == 'POST':
         new_title = request.form['title']
-        new_blog = request.form['blog']
-        new_post = Blog(new_title, new_blog) 
-        db.session.add(new_post)
-        db.session.commit()
-        return redirect ('/blog?id={}'.format(new_post.id)) 
+        new_blog = request.form['blog'] 
 
-    return render_template('newpost.html', title="Add a Blog Entry")
+        if new_title == "":
+            flash('Must have a title')
+            return redirect("/newpost")
+        if new_blog == "":
+            flash('Must have a body')
+            return redirect("/newpost")
+        else:
+            new_post = Blog(title, blog)
+            db.session.add(new_post)
+            db.session.commit()
+            return redirect ('/blog?id={}'.format(new_post.id))
+        
+
+    return render_template('newpost.html', title="Add an Entry")
 
 @app.route('/blog', methods=['POST', 'GET'])
 def blog(): 
@@ -43,9 +52,9 @@ def blog():
     if blog_id == None:
         blogs = Blog.query.all()
         return render_template('blog.html', title="Build a Blog", blogs=blogs)
-    else: 
+    else:
         posts = Blog.query.get(blog_id)
-        return render_template('blogpost.html', title='Your New Post', posts=posts)
+        return render_template('blogpost.html', title=posts.title, posts=posts)
 
 if __name__ == '__main__':
     app.run()
